e004dd535509a50cf3287d4baf0c520453497feb
bensoutendijk <bensoutendijk@gmail.com>
2020-08-09 18:41:25 -0500

    update proxy settings

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = (app) => {
   if (process.env.NODE_ENV === 'production') {
-    app.use(proxy('/api', { target: 'https://soutendijk-starter.herokuapp.com/' }));
+    app.use(proxy('/api', { target: 'https://soutendijk-billboard.herokuapp.com/' }));
   } else {
     app.use(proxy('/api', { target: 'http://localhost:5000/' }));
   }
