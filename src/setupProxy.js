202526c97db50dafbdebbd88a40aaed9107aba20
Ben Soutendijk <bensoutendijk@gmail.com>
2020-12-17 11:43:28 -0600

    update proxy to  use heroku

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = (app) => {
   if (process.env.NODE_ENV === 'production') {
-    app.use(proxy('/api', { target: 'https://billboard-soutendijk.azurewebsites.net/' }));
+    app.use(proxy('/api', { target: 'https://billboard-soutendijk.herokuapp.com/' }));
   } else {
     app.use(proxy('/api', { target: 'http://localhost:5000/' }));
   }
