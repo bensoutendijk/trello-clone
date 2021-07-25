cd60086e98e0dc8ed6e76f67702641c194d61960
Ben Soutendijk <bensoutendijk@gmail.com>
2021-07-24 23:49:31 -0500

    fix proxy settings for docker

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,8 +2,8 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = (app) => {
   if (process.env.NODE_ENV === 'production') {
-    app.use(proxy('/api', { target: 'https://billboard-soutendijk.herokuapp.com/' }));
+    app.use(proxy('/api', { target: 'http://api:5000' }));
   } else {
-    app.use(proxy('/api', { target: 'http://localhost:5000/' }));
+    app.use(proxy('/api', { target: 'http://api:5000' }));
   }
 };
